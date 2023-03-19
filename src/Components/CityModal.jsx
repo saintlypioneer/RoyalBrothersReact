b367317261d00da900faa230c432477219202680
harshadapr <harshadapraundal97@gmail.com>
2023-03-19 22:11:31 +0530

    updated citymodaland blog page

--- src/Components/CityModal.jsx
+++ src/Components/CityModal.jsx
@@ -42,7 +42,7 @@ function CityModal({ isOpen, setIsOpen, setSelectedCity }) {
                 "https://d3vp2rl7047vsp.cloudfront.net/cities/round_images/000/000/039/original/Agra_new.jpg",
         },
         {
-            name: "Ahemdabad",
+            name: "Haridwar",
             url: "https://d3vp2rl7047vsp.cloudfront.net/cities/round_images/000/000/023/original/Ahmedabad_b8d1eb5783c996a077aff887d35e4168.jpg?1558599488"
         },
 
@@ -55,12 +55,36 @@ function CityModal({ isOpen, setIsOpen, setSelectedCity }) {
             url: "https://d3vp2rl7047vsp.cloudfront.net/cities/round_images/000/000/101/original/Amritsar.jpg?1568037649"
         },
         {
-            name: "Andaman",
+            name: "Goa",
             url: "https://d3vp2rl7047vsp.cloudfront.net/cities/round_images/000/000/017/original/Andaman-_-Nicobar_b5a3ba5c464968f274dfb7a98bcc4dbc.jpg?1558599551"
         },
         {
             name: "chandigarh",
             url: "https://d3vp2rl7047vsp.cloudfront.net/cities/round_images/000/000/024/original/Chandigarh_72374d6c6de8b70d5b4ea5c05c59ba32.jpg?1558599669"
+        },
+        {
+            name: "Banglore",
+            url: "https://d3vp2rl7047vsp.cloudfront.net/cities/round_images/000/000/001/original/Bangalore_1cf1aa53741a94edc3532f7b2b583c52.jpg?1558599565"
+        },
+        {
+            name: "Kolkata",
+            url: "https://d3vp2rl7047vsp.cloudfront.net/cities/round_images/000/000/095/original/Guwahati_3adb5736296c93fec082f24b87dce70d.jpg?1558600028"
+        },
+        {
+            name: "Belagam",
+            url: "https://d3vp2rl7047vsp.cloudfront.net/cities/round_images/000/000/103/original/Belagaum.jpg?1576578730"
+        },
+        {
+            name: "manali",
+            url: "https://d3vp2rl7047vsp.cloudfront.net/cities/round_images/000/000/028/original/Bankok_efd1b11328e1ac61488cac98a15dc584.jpg?1558599580"
+        },
+        {
+            name: "Ahemdabad",
+            url: "https://d3vp2rl7047vsp.cloudfront.net/cities/round_images/000/000/012/original/Hyderabad_e23f5904385e0c21afd0fe4b7ff698fb.jpg?1558600101"
+        },
+        {
+            name: "Jaislmer",
+            url: "https://d3vp2rl7047vsp.cloudfront.net/cities/round_images/000/000/035/original/Jaisalmer_803980ec3a977dd61c215c5fbefd3909.jpg?1558600142"
         }
 
     ];
@@ -92,7 +116,7 @@ function CityModal({ isOpen, setIsOpen, setSelectedCity }) {
                         </InputGroup>
                     </Box>
                     <ModalBody>
-                        <Grid templateColumns="repeat(4, 1fr)" gap={6} gridGap={6} >
+                        <Grid templateColumns="repeat(auto-fit, minmax(180px, 1fr))" gap={6} gridGap={6} >
                             {filteredImages.map((image) => (
                                 <GridItem key={image.name} borderRadius={"5px "} overflow="hidden" onClick={() => { handleCitySelect(`${image.name}`) }}>
                                     <Image src={image.url} />
@@ -123,7 +147,7 @@ const WiggleText = styled.p`
 
 
     &:hover{
-        animation: wiggle 2.5s infinite;
+        animation: wiggle 1.5s infinite;
     }
 
 

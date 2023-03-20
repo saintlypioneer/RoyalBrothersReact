332523f2d036f88a84501c8410d3925fd6b88311
vishwajeet-7 <vishady7@gmail.com>
2023-03-20 14:42:16 +0530

    updated navigation

--- src/Components/PrivateRoute/PrivateRoute.jsx
+++ src/Components/PrivateRoute/PrivateRoute.jsx
@@ -1,10 +1,10 @@
 import React, { Children } from 'react'
 import { useSelector } from 'react-redux'
-import { Navigate,useLocation  } from 'react-router-dom';
+import { Navigate  } from 'react-router-dom';
 
-export default function PrivateRoute({children,...rest}) {
-  const {isLoggedIn} = useSelector((store)=>store.LoginSignupRed);
-  const location = useLocation()
+export default function PrivateRoute({children}) {
+  const {isLoggedIn} = useSelector((store)=>store.LoginSignupRed.useDetails);
+  // const location = useLocation()
   
 
   //<<<<<<<<< Trying redirecting to last path >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
